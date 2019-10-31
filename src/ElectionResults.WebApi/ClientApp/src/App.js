5dfcaeb0fd235184d061e69ea4237128d080439a
ascripcaru <andrei.scripcaru@pure360.com>
2019-10-31 13:30:46 +0200

    create admin panel to edit data source

--- src/ElectionResults.WebApi/ClientApp/src/App.js
+++ src/ElectionResults.WebApi/ClientApp/src/App.js
@@ -5,6 +5,7 @@ import { Layout } from "./components/Layout";
 import { ChartContainer } from "./components/CandidatesChart";
 import { FetchData } from "./components/FetchData";
 import { Counter } from "./components/Counter";
+import { AdminPanel } from "./components/AdminPanel/AdminPanel";
 
 export default class App extends Component {
   static displayName = App.name;
@@ -15,6 +16,7 @@ export default class App extends Component {
         <Route exact path="/" component={ChartContainer} />
         <Route path="/counter" component={Counter} />
         <Route path="/fetch-data" component={FetchData} />
+        <Route path="/admin" component={AdminPanel} />
       </Layout>
     );
   }
